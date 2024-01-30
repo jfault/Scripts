9453cae9d402f44b587ce5dfdfc9fe019d768b5b
J <114438759+aerxx@users.noreply.github.com>
2024-01-30 12:28:48 -0600

    Add files via upload

---
+++ Remediation/Detect-NETBIOS.ps1
@@ -0,0 +1,16 @@
+﻿$interfaces = Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces\TCPIP*
+$i = 0
+ 
+foreach ($interface in $interfaces) {
+    $i +=  $interface.NetbiosOptions
+}
+ 
+if ($interfaces.Count *2 -eq $i) {
+    #netbios disabled
+    return "exit 0"
+}
+ 
+else {
+    # netbios enabled
+    return "exit 1"
+}
---
+++ Remediation/Remediate-NETBIOS.ps1
@@ -0,0 +1,4 @@
+﻿$NICS = Get-WmiObject win32_NetworkAdapterConfiguration
+foreach ($NIC in $NICS){
+        $NIC.settcpipnetbios(2) # 2 = disable netbios on interface
+    }
