47cc1c8952213345decd541dab6427374b8ea872
J <114438759+aerxx@users.noreply.github.com>
2024-01-30 12:34:25 -0600

    Update Remediate-NETBIOS.ps1
    Added description into code.

--- Remediation/Remediate-NETBIOS.ps1
+++ Remediation/Remediate-NETBIOS.ps1
@@ -1,4 +1,16 @@
-﻿$NICS = Get-WmiObject win32_NetworkAdapterConfiguration
+<#
+Version: 1.0
+Author: 
+- Jeremy https://twitter.com/Jeremy_reboot
+Script: Remediate-NETBIOS
+Description: Script Remediates NETBIOS if it is enabled, if not enabled no action is taken.
+Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
+Run this script using the logged-on credentials: Yes
+Enforce script signature check: No
+Run script in 64-bit PowerShell: Yes
+#> 
+ 
+$NICS = Get-WmiObject win32_NetworkAdapterConfiguration
 foreach ($NIC in $NICS){
         $NIC.settcpipnetbios(2) # 2 = disable netbios on interface
     }
