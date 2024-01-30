306c07fee932445df0c7de5d9a5485431a45b549
J <114438759+aerxx@users.noreply.github.com>
2024-01-30 12:33:01 -0600

    Update Detect-NETBIOS.ps1
    Added description within code

--- Remediation/Detect-NETBIOS.ps1
+++ Remediation/Detect-NETBIOS.ps1
@@ -1,4 +1,16 @@
-﻿$interfaces = Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces\TCPIP*
+<#
+Version: 1.0
+Author: 
+- Jeremy https://twitter.com/Jeremy_reboot
+Script: Detect-NETBIOS
+Description: Script detects whether or not NETBIOS is enabled on assigned device(s)
+Hint: This is a community script. There is no guarantee for this. Please check thoroughly before running.
+Run this script using the logged-on credentials: Yes
+Enforce script signature check: No
+Run script in 64-bit PowerShell: Yes
+#> 
+ 
+$interfaces = Get-ItemProperty HKLM:\SYSTEM\CurrentControlSet\Services\NetBT\Parameters\Interfaces\TCPIP*
 $i = 0
  
 foreach ($interface in $interfaces) {
